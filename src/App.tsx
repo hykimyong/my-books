174f1102a6a2ce8b85c5f7490dd02f17bd7a8c71
hykimyong <hykimyong@naver.com>
2023-09-25 21:25:37 +0900

    route setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Add from './pages/Add';
+import Detail from './pages/Detail';
+import Edit from './pages/Edit';
+import Notfound from './pages/Notfound';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/*" element={<Notfound/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
